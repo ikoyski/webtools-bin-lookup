b1b6be13b2957f25f0a32be8080603dd3337ffa5
CRSanpedro <CRSanpedro@F10220.rcgitmanila.com>
2022-07-26 13:01:32 +0800

    refactored

--- src/main/java/com/ikoyski/webtoolsbinlookup/provider/BinLookupProviderBinList.java
+++ src/main/java/com/ikoyski/webtoolsbinlookup/provider/BinLookupProviderBinList.java
@@ -1,16 +1,29 @@
 package com.ikoyski.webtoolsbinlookup.provider;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ikoyski.webtoolsbinlookup.dto.BinLookupResponse;
 
 public class BinLookupProviderBinList implements BinLookupProviderBaseInterface {
 
+	public static final String ERROR_INVALID_URI = "invalid uri";
+
 	@Override
 	public BinLookupResponse getBinInfo(String bin) {
-		String uri = "https://lookup.binlist.net/" + bin;
-		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.getForObject(uri, BinLookupResponse.class);
+		BinLookupResponse binLookupResponse = null;
+		try {
+			URI uri = new URI("https://lookup.binlist.net/" + bin);
+			RestTemplate restTemplate = new RestTemplate();
+			binLookupResponse = restTemplate.getForObject(uri, BinLookupResponse.class);
+		} catch (URISyntaxException e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, ERROR_INVALID_URI, null);
+		}
+		return binLookupResponse;
 	}
 
 }
