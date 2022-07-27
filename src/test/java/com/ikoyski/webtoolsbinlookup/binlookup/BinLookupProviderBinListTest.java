d6df08ae40957aaf2d4f22a1dc0aa45d676ecdd7
ikoyski <ikoyski@ikoyski-laptop>
2022-07-28 02:04:30 +0800

    added more tests

---
+++ src/test/java/com/ikoyski/webtoolsbinlookup/binlookup/BinLookupProviderBinListTest.java
@@ -0,0 +1,46 @@
+package com.ikoyski.webtoolsbinlookup.binlookup;
+
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ikoyski.webtoolsbinlookup.dto.BinLookupResponse;
+import com.ikoyski.webtoolsbinlookup.provider.BinLookupProviderBaseInterface;
+import com.ikoyski.webtoolsbinlookup.provider.BinLookupProviderFactory;
+
+@SpringBootTest
+class BinLookupProviderBinListTest {
+
+	@Test
+	@DisplayName("BinLookupProviderBinListTest.binLookupProviderBinListSuccess()")
+	void binLookupProviderBinListSuccess() {
+		// given
+		final String BIN = "542458";
+		BinLookupProviderFactory binLookupProviderFactory = new BinLookupProviderFactory();
+		BinLookupProviderBaseInterface binLookupProvider = binLookupProviderFactory
+				.createBinLookupProvider(BinLookupProviderFactory.PROVIDER_BINLIST);
+
+		// when
+		BinLookupResponse binLookupResponse = binLookupProvider.getBinInfo(BIN);
+
+		// then
+		Assertions.assertNotNull(binLookupResponse);
+	}
+
+	@Test
+	@DisplayName("BinLookupProviderBinListTest.binLookupProviderBinListException()")
+	void binLookupProviderBinListException() {
+		// given
+		final String BIN = "\\";
+		BinLookupProviderFactory binLookupProviderFactory = new BinLookupProviderFactory();
+		BinLookupProviderBaseInterface binLookupProvider = binLookupProviderFactory
+				.createBinLookupProvider(BinLookupProviderFactory.PROVIDER_BINLIST);
+
+		// when and then
+		Assertions.assertThrows(Exception.class, () -> binLookupProvider.getBinInfo(BIN));
+	}
+
+}
