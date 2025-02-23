5f020b12173c6e50d1d5e6b6c3f95654c43c5353
CRSanpedro <CRSanpedro@2023RLD0385-LT.rcgitmanila.com>
2025-02-23 10:15:28 -0800

    on localhost: service registry, config server connection is ok

--- src/main/java/com/ikoyski/webtools/binlookup/WebtoolsBinLookupApplication.java
+++ src/main/java/com/ikoyski/webtools/binlookup/WebtoolsBinLookupApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class WebtoolsBinLookupApplication {
+public class WebtoolsBinLookupApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebtoolsBinLookupApplication.class, args);
--- src/test/java/com/ikoyski/webtools/binlookup/BinLookupServiceTest.java
+++ src/test/java/com/ikoyski/webtools/binlookup/BinLookupServiceTest.java
@@ -1,27 +1,21 @@
 package com.ikoyski.webtools.binlookup;
 
-import static org.mockito.Mockito.doReturn;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.ActiveProfiles;
 
-import com.ikoyski.webtools.binlookup.dto.BinLookupCountry;
 import com.ikoyski.webtools.binlookup.dto.BinLookupResponse;
-import com.ikoyski.webtools.binlookup.provider.BinLookupProviderBaseInterface;
 import com.ikoyski.webtools.binlookup.service.BinLookupService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BinLookupServiceTest {
 
 	@Autowired
 	private BinLookupService binLookupService;
-
-	@MockitoBean
-	BinLookupProviderBaseInterface binLookupProvider;
 
 	@Test
 	@DisplayName("BinLookupServiceTest.getBinInfoSuccess()")
@@ -29,17 +23,12 @@ class BinLookupServiceTest {
 		// given
 		final String BIN = "542458";
 		final String BIN_COUNTRY = "PH";
-		BinLookupResponse binLookupResponse = new BinLookupResponse();
-		BinLookupCountry binLookupCountry = new BinLookupCountry();
-		binLookupCountry.setAlpha2(BIN_COUNTRY);
-		binLookupResponse.setCountry(binLookupCountry);
-		doReturn(binLookupResponse).when(binLookupProvider).getBinInfo(BIN);
 
 		// when
-		BinLookupResponse binLookupResponse2 = binLookupService.getBinInfo(BIN);
+		BinLookupResponse binLookupResponse = binLookupService.getBinInfo(BIN);
 
 		// then
-		Assertions.assertEquals(binLookupResponse2.getCountry().getAlpha2(), BIN_COUNTRY);
+		Assertions.assertEquals(binLookupResponse.getCountry().getAlpha2(), BIN_COUNTRY);
 	}
 
 }
