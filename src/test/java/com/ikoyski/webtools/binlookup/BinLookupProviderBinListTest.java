d738a529c74227c6c10bab62453749ecba4f2be2
ikoyski <christian_sanpedro@yahoo.com>
2025-02-24 11:00:57 +0000

    now working on ubuntuserver01

--- src/test/java/com/ikoyski/webtools/binlookup/BinLookupProviderBinListTest.java
+++ src/test/java/com/ikoyski/webtools/binlookup/BinLookupProviderBinListTest.java
@@ -3,21 +3,26 @@ package com.ikoyski.webtools.binlookup;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.ikoyski.webtools.binlookup.dto.BinLookupResponse;
 import com.ikoyski.webtools.binlookup.provider.BinLookupProviderBaseInterface;
 import com.ikoyski.webtools.binlookup.provider.BinLookupProviderFactory;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BinLookupProviderBinListTest {
+
+	@Autowired
+	BinLookupProviderFactory binLookupProviderFactory;
 
 	@Test
 	@DisplayName("BinLookupProviderBinListTest.binLookupProviderBinListSuccess()")
 	void binLookupProviderBinListSuccess() {
 		// given
 		final String BIN = "542458";
-		BinLookupProviderFactory binLookupProviderFactory = new BinLookupProviderFactory();
 		BinLookupProviderBaseInterface binLookupProvider = binLookupProviderFactory
 				.createBinLookupProvider(BinLookupProviderFactory.PROVIDER_BINLIST);
 
@@ -33,7 +38,6 @@ class BinLookupProviderBinListTest {
 	void binLookupProviderBinListException() {
 		// given
 		final String BIN = "\\";
-		BinLookupProviderFactory binLookupProviderFactory = new BinLookupProviderFactory();
 		BinLookupProviderBaseInterface binLookupProvider = binLookupProviderFactory
 				.createBinLookupProvider(BinLookupProviderFactory.PROVIDER_BINLIST);
 
